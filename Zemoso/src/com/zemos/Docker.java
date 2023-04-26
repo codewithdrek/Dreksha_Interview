a2f18b1d628444f8f01cdd847ed4e56ed7bce1ea
Admin <you@example.com>
2023-04-26 10:54:56 +0530

    Function

--- Zemoso/src/com/zemos/Docker.java
+++ Zemoso/src/com/zemos/Docker.java
@@ -9,5 +9,16 @@ public class Docker {
 	//By default size is 16 
 	//Default method y it has come 
 	//can thread called twice : yes 2 lifecycle will start
+	//API GATEWAY
+	//Linke dlist cicular or loop
+	//Distributed logs
+	//Avoid lazy and eager
+	//live lock
+	//Factory design [pa
+	//BFS and dfs
+	//minum spanning tree
+	//copyonWrite 
+	//hashmapo and concurrent hashmap
+	//y clone is prresent inobject clas y not in Thread class
 	//
 }
